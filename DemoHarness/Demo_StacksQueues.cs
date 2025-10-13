4a86c1108b2633c646c308183553a3814c954d1e
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-10-12 20:31:14 -0700

    Added Demo Harness

---
+++ DemoHarness/Demo_StacksQueues.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataStructuresToolkit.StackQueues;
+
+namespace DemoHarness
+{
+    public class Demo_StacksQueues
+    {
+        public static void Run()
+        {
+            Console.WriteLine("=== STACK DEMO: Undo Sequence (LIFO) ===");
+            var undoStack = new MyStack<string>();
+            undoStack.Push("Typed 'Hello'");
+            undoStack.Push("Bolded text");
+            undoStack.Push("Inserted image");
+
+            while (undoStack.Count > 0)
+            {
+                Console.WriteLine($"Undo: {undoStack.Pop()}");
+            }
+
+            Console.WriteLine("\n=== QUEUE DEMO: Print Job Order (FIFO) ===");
+            var printQueue = new MyQueue<string>();
+            printQueue.Enqueue("Job1: Resume");
+            printQueue.Enqueue("Job2: Invoice");
+            printQueue.Enqueue("Job3: Report");
+
+            while (printQueue.Count > 0)
+            {
+                Console.WriteLine($"Printing: {printQueue.Dequeue()}");
+            }
+        }
+    }
+}
